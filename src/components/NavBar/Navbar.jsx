3a4c4f9205e65e61aa24bfef569e67f3b04e8a0e
Abhinav Gupta <abhinav6483@gmail.com>
2025-01-15 01:18:52 +0530

    added hamburger

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,19 +1,24 @@
 import { Link, NavLink } from "react-router-dom";
+import { useState } from "react";
 import "./NavbarStyle.css";
 
 function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
         {/* app Logo */}
-
-        <Link to="/" className="navbar-logo hide-on-small">
+        <Link to="/" className="navbar-logo">
           🎥 MovieBrowser
         </Link>
 
         {/* Navlinks */}
-
-        <ul className="navbar-links">
+        <ul className={`navbar-links ${isMenuOpen ? 'active' : ''}`}>
           <li>
             <NavLink
               to="/"
@@ -22,6 +27,7 @@ function Navbar() {
                   isActive ? "text-orange-700" : "text-white"
                 }  hover:text-orange-700`
               }
+              onClick={() => setIsMenuOpen(false)}
             >
               Home
             </NavLink>
@@ -32,8 +38,9 @@ function Navbar() {
               className={({ isActive }) =>
                 `navbar-link ${
                   isActive ? "text-orange-700" : "text-white"
-                }  hover:text-orange-700 `
+                }  hover:text-orange-700`
               }
+              onClick={() => setIsMenuOpen(false)}
             >
               Search
             </NavLink>
@@ -44,13 +51,24 @@ function Navbar() {
               className={({ isActive }) =>
                 `navbar-link ${
                   isActive ? "text-orange-700" : "text-white"
-                }  hover:text-orange-700 `
+                }  hover:text-orange-700`
               }
+              onClick={() => setIsMenuOpen(false)}
             >
               Favourites
             </NavLink>
           </li>
         </ul>
+
+        {/* Hamburger Menu */}
+        <div 
+          className={`hamburger ${isMenuOpen ? 'active' : ''}`} 
+          onClick={toggleMenu}
+        >
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
       </div>
     </nav>
   );
